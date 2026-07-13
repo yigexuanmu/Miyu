b590f3e532b80d96e1303a743e0b875e4df856f8
yigexuanmu <yigexuanmu@users.noreply.github.com>
2026-07-13 23:07:58 +0800

    sync: update to upstream v0.1.18

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
 
       packages.default = pkgs.rustPlatform.buildRustPackage {
         pname = "miyu";
-        version = "0.1.17";
+        version = "0.1.18";
         src = miyu-src;
         cargoLock.lockFile = "${miyu-src}/Cargo.lock";
         nativeBuildInputs = [ pkgs.pkg-config ];
